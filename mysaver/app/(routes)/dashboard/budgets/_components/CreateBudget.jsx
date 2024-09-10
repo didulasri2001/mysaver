1a0ff61b886fc4b1b7d074bbbd081d7a70646fd4
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-10 23:03:35 +0530

    feat: add budget dialog box

--- mysaver/app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ mysaver/app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,12 +1,34 @@
+"use client";
 import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function CreateBudget() {
   return (
     <div>
-      <div className="bg-[#f5f1f1] p-10 rounded-md justify-center text-center border-2 cursor-pointer hover:shadow-md mt-5">
-        <h2 className="text-3xl">+</h2>
-        <h2>Create New Budget</h2>
-      </div>
+      <Dialog>
+        <DialogTrigger asChild>
+          <div className="bg-[#f5f1f1] p-10 rounded-md justify-center text-center border-2 cursor-pointer hover:shadow-md mt-5">
+            <h2 className="text-3xl">+</h2>
+            <h2>Create New Budget</h2>
+          </div>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Create New Budget</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
