0db750711e5f9993defe2eaa52a491f06b08a595
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-11 16:05:54 +0530

    feat: budget save in database

--- mysaver/app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
+++ mysaver/app/(routes)/dashboard/budgets/_components/CreateBudget.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -9,22 +9,87 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { Button } from "@/components/ui/button";
+import { db } from "@/Utils/dbConfig";
+import { Budgets } from "@/Utils/schema";
+import { useUser } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
+import { Input } from "@/components/ui/input";
+
 function CreateBudget() {
+  const { user } = useUser();
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handlebudget = async () => {
+    setOpen(false);
+
+    const result = await db
+      .insert(Budgets)
+      .values({
+        name: name,
+        amount: amount,
+        createdBy: user.primaryEmailAddress?.emailAddress, // Ensure user email is available
+      })
+      .returning({ insertedId: Budgets.id });
+
+    console.log(result);
+
+    //   if (result) {
+    //     Toaster("Budget Created Successfully");
+    //     setOpen(false); // Close the dialog after successful creation
+    //   } else {
+    //     Toaster("Failed to create budget");
+    //   }
+    // } catch (error) {
+    //   console.error("Error creating budget:", error);
+    //   Toaster("Error occurred while creating budget");
+    // }
+  };
+
   return (
     <div>
-      <Dialog>
+      <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
           <div className="bg-[#f5f1f1] p-10 rounded-md justify-center text-center border-2 cursor-pointer hover:shadow-md mt-5">
             <h2 className="text-3xl">+</h2>
             <h2>Create New Budget</h2>
           </div>
         </DialogTrigger>
-        <DialogContent>
+
+        <DialogContent className="bg-[#f5f1f1] p-6 rounded-md">
           <DialogHeader>
             <DialogTitle>Create New Budget</DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <div className="grid gap-4">
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Name</h2>
+                  <Input
+                    type="text"
+                    placeholder="Enter Budget"
+                    className="p-2 border"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                  <Input
+                    type="number"
+                    placeholder="Enter Amount"
+                    className="p-2 border"
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </div>
+
+                <Button
+                  disabled={!name.trim() || !amount.trim()}
+                  className="bg-[#f4b3b0] text-black py-2 px-4 rounded-md hover:bg-[#f4b3b0]"
+                  onClick={() => handlebudget()}
+                >
+                  Create
+                </Button>
+              </div>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
