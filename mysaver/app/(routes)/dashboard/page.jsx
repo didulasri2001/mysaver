26b8393d93f3983384909ebc87d6ee5659e9f283
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-15 17:45:30 +0530

    feat: styles to dashboard page

--- mysaver/app/(routes)/dashboard/page.jsx
+++ mysaver/app/(routes)/dashboard/page.jsx
@@ -6,7 +6,9 @@ function Dashboard() {
   const { user } = useUser();
   return (
     <div className="p-5">
-      <h2 className="font-bold text-3xl">Hi, {user?.fullName} 👋</h2>
+      <h2 className="font-bold text-3xl text-gray-500">
+        Hi, {user?.fullName} 👋
+      </h2>
     </div>
   );
 }
