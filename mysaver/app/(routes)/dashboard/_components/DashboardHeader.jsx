b5c4bd1ccfc9d556de5dfb54e33729ef008461ea
Lakpriya D.D.S <lakpriyadds.21@uom.lk>
2024-09-16 08:56:23 +0530

    feat: add styles to dashboard

--- mysaver/app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ mysaver/app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -4,7 +4,6 @@ import React from "react";
 function DashboardHeader() {
   return (
     <div className="p-5 shadow-sm border-b flex justify-between">
-      <div>Search Bar</div>
       <div>
         <UserButton />
       </div>
